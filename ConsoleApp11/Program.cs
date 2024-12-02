9ed688f6ffd24527928ec6e22144559537bc0548
VladimirSivac <morndes@gmail.com>
2024-12-02 21:27:59 +0200

    Task-1 done

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -7,11 +7,33 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp11
 {
+    /// <summary>
+    /// Centimeters
+    /// </summary>
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            int m = Input("Количество метров: ");
+            int c = Input("Количество сантиметров: ");
+            Output(Centimeters(m, c));
+            Console.ReadKey();
+        }
+
+        static int Input(string message)
+        {
+            Console.Write(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static int Centimeters(int m, int c)
+        {
+            return m * 100 + c;
+        }
+        
+        static void Output(int result)
+        {
+            Console.WriteLine($"Всего сантиметров: {result}");
         }
     }
 }
